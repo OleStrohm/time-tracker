13c6e542d6346a3b2e7a0a3dd56ec0c20dd574b9
Ole Strohm <ole@strohm.no>
2026-04-04 12:07:05 +0200

    Create time tracker plugin

---
+++ flake.nix
@@ -0,0 +1,25 @@
+{
+  description = "Rust flake with nightly";
+
+  inputs = {
+    flake-utils.url = "github:numtide/flake-utils";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+  };
+
+  outputs = { flake-utils, nixpkgs, ... }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = (import nixpkgs) {
+          inherit system;
+        };
+      in
+      with pkgs;
+      {
+        devShells.default = mkShell {
+          nativeBuildInputs = [
+            nodejs
+          ];
+        };
+      }
+    );
+}
--- src/main.ts
+++ src/main.ts
@@ -1,99 +1,54 @@
-import {App, Editor, MarkdownView, Modal, Notice, Plugin} from 'obsidian';
-import {DEFAULT_SETTINGS, MyPluginSettings, SampleSettingTab} from "./settings";
+import {Plugin} from 'obsidian';
 
-// Remember to rename these classes and interfaces!
-
-export default class MyPlugin extends Plugin {
-	settings: MyPluginSettings;
-
-	async onload() {
-		await this.loadSettings();
-
-		// This creates an icon in the left ribbon.
-		this.addRibbonIcon('dice', 'Sample', (evt: MouseEvent) => {
-			// Called when the user clicks the icon.
-			new Notice('This is a notice!');
-		});
-
-		// This adds a status bar item to the bottom of the app. Does not work on mobile apps.
-		const statusBarItemEl = this.addStatusBarItem();
-		statusBarItemEl.setText('Status bar text');
-
-		// This adds a simple command that can be triggered anywhere
-		this.addCommand({
-			id: 'open-modal-simple',
-			name: 'Open modal (simple)',
-			callback: () => {
-				new SampleModal(this.app).open();
-			}
-		});
-		// This adds an editor command that can perform some operation on the current editor instance
-		this.addCommand({
-			id: 'replace-selected',
-			name: 'Replace selected content',
-			editorCallback: (editor: Editor, view: MarkdownView) => {
-				editor.replaceSelection('Sample editor command');
-			}
-		});
-		// This adds a complex command that can check whether the current state of the app allows execution of the command
-		this.addCommand({
-			id: 'open-modal-complex',
-			name: 'Open modal (complex)',
-			checkCallback: (checking: boolean) => {
-				// Conditions to check
-				const markdownView = this.app.workspace.getActiveViewOfType(MarkdownView);
-				if (markdownView) {
-					// If checking is true, we're simply "checking" if the command can be run.
-					// If checking is false, then we want to actually perform the operation.
-					if (!checking) {
-						new SampleModal(this.app).open();
-					}
-
-					// This command will only show up in Command Palette when the check function returns true
-					return true;
-				}
-				return false;
-			}
-		});
-
-		// This adds a settings tab so the user can configure various aspects of the plugin
-		this.addSettingTab(new SampleSettingTab(this.app, this));
-
-		// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
-		// Using this function will automatically remove the event listener when this plugin is disabled.
-		this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
-			new Notice("Click");
-		});
-
-		// When registering intervals, this function will automatically clear the interval when the plugin is disabled.
-		this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
-
+function legend_colors(legend: string): Map {
+	let color_map = new Map();
+	const legend_rows = legend.split('\n').filter((row) => row.length > 0);
+	for (let r = 0; r < legend_rows.length; r++) {
+		let row = legend_rows[r].split(' ');
+		let character = row[1];
+		let color = row[2];
+		color_map.set(character, color);
 	}
+	return color_map;
+}
 
-	onunload() {
-	}
-
-	async loadSettings() {
-		this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData() as Partial<MyPluginSettings>);
-	}
-
-	async saveSettings() {
-		await this.saveData(this.settings);
+function char_color(color_map: Map, character: string): string {
+	if (color_map.has(character)) {
+		return color_map.get(character);
+	} else {
+		return 'var(--background-primary)';
 	}
 }
 
-class SampleModal extends Modal {
-	constructor(app: App) {
-		super(app);
+export default class MyPlugin extends Plugin {
+	processor: MarkdownPostProcessor;
+
+	async onload() {
+		this.processor = this.registerMarkdownCodeBlockProcessor('time-tracker', (source, el, ctx) => {
+			const parts = source.split('\nLegend:\n');
+			const data = parts[0];
+			const legend = parts[1];
+			const color_map = legend_colors(legend);
+			const rows = data.split('\n').filter((row) => row.length > 0);
+
+			const body = el.createEl('div', { cls: 'monospace' });
+
+			for (let r = 0; r < rows.length; r++) {
+				const row = body.createEl('div');
+				if (rows[r] == "      0123456789TE0123456789TE") {
+					row.createEl('span', { text: rows[r] });
+				} else {
+					for (let c = 0; c < rows[r].length; c++) {
+						let character = rows[r][c];
+						let attr = { 'style': 'background-color: ' + char_color(color_map, character) + ';' };
+						row.createEl('span', { text: character, attr: attr});
+					}
+				}
+			}
+		});
 	}
 
-	onOpen() {
-		let {contentEl} = this;
-		contentEl.setText('Woah!');
-	}
-
-	onClose() {
-		const {contentEl} = this;
-		contentEl.empty();
+	onunload() {
+		this.unregisterPostProcessor(this.processor);
 	}
 }
